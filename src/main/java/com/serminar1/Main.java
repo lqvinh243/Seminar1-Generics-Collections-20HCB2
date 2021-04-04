e2734ad8e0ea0b7409ba6ba69b555dde327d2348
Dylan <lqvinh243@gmail.com>
2021-04-04 18:46:07 +0700

    add data slag code suong suong

--- src/main/java/com/serminar1/Main.java
+++ src/main/java/com/serminar1/Main.java
@@ -5,10 +5,114 @@
  */
 package com.serminar1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.Stack;
+import java.util.TreeMap;
+
 /**
  *
  * @author lqvin
  */
 public class Main {
-    
+
+    static Stack<String> his = new Stack<>();
+
+    public static TreeMap<String, ArrayList<String>> ReadSlagWork() {
+        TreeMap<String, ArrayList<String>> map = new TreeMap<String, ArrayList<String>>();
+        try {
+            File myObj = new File("slang.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] arrOfStr = data.split("`", 2);
+                if (arrOfStr.length < 2) {
+                    continue;
+                }
+                String lang = arrOfStr[1];
+                String[] g = lang.split("\\|");
+                map.put(arrOfStr[0], new ArrayList<String>(Arrays.asList(g)));
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        return map;
+    }
+
+    public static ArrayList<String> findBySlagWord(TreeMap<String, ArrayList<String>> map, String slagWord) {
+        his.push(slagWord);
+        return map.get(slagWord);
+    }
+
+    public static void findByDefinition(TreeMap<String, ArrayList<String>> map, String slagWord) {
+        for (Map.Entry<String, ArrayList<String>> entry : map.entrySet()) {
+            if (useLoop(entry.getValue(), slagWord)) {
+                System.out.println(entry.getKey());
+            }
+        }
+    }
+
+    public static boolean useLoop(ArrayList<String> arr, String targetValue) {
+        for (String s : arr) {
+            if (s.contains(targetValue)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void randomMap(TreeMap<String, ArrayList<String>> map) {
+        Set<String> keySet = map.keySet();
+        List<String> keyList = new ArrayList<>(keySet);
+        int size = keyList.size();
+        int randIdx = new Random().nextInt(size);
+//
+        String randomKey = keyList.get(randIdx);
+        ArrayList<String> randomValue = map.get(randomKey);
+
+        System.out.println("key: " + randomKey + ", value: " + randomValue);
+    }
+
+    public static void deleteSlagWord(TreeMap<String, ArrayList<String>> map) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Nhap slag word can xoa:");
+        String slagWord = scan.nextLine();
+        if (map.containsKey(slagWord) == false) {
+            System.out.println("Khong tim thay slag word nay!");
+            return;
+        }
+        System.out.println("Ban co chac muon xoa slag word nay?");
+        System.out.println("1.Yes?");
+        System.out.println("0.No?");
+        String choose = scan.nextLine();
+        switch (choose) {
+            case "0":
+                break;
+            case "1":
+                map.remove(slagWord);
+                break;
+            default:
+                System.out.println("Khong co lua chon nay!");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeMap<String, ArrayList<String>> map = ReadSlagWork();
+        long startTime = System.nanoTime();
+        deleteSlagWord(map);
+        long endTime = System.nanoTime();
+        long timeElapsed = endTime - startTime;
+        System.out.println("Execution time in milliseconds: " + timeElapsed / 1000000);
+    }
 }
